83908cafe5b792395b37eb73265b59fc236ef80d
GitFLHub <1390491039@qq.com>
2022-09-21 20:52:04 +0800

    v0.0.4

--- web/memory/memory.go
+++ web/memory/memory.go
@@ -9,20 +9,85 @@ import (
 )
 
 type Provider struct {
-	lock     sync.Locker              // 用来锁
-	sessions map[string]*list.Element // 存储在内存中
-	list     *list.List               // list 是 package List 是 Structure
+	// 用来锁
+	lock sync.Locker
+	// 存储在内存中 map 里存放的是指针
+	sessions map[string]*list.Element
+	// list 是 package List 是 Structure 用于GC？
+	list *list.List
 }
+
+var pder = &Provider{list: list.New()}
 
 func (pder *Provider) SessionInit(sid string) (session.Session, error) {
 	pder.lock.Lock()
 	defer pder.lock.Unlock()
 	v := make(map[interface{}]interface{}, 0)
 	newsess := &SessionStore{sid: sid, timeAccessed: time.Now(), value: v}
-	ele
+	element := pder.list.PushBack(newsess)
+	pder.sessions[sid] = element
+	return newsess, nil
 }
 
-var pder = &Provider{list: list.New()}
+func (pder *Provider) SessionRead(sid string) (session.Session, error) {
+	if element, ok := pder.sessions[sid]; ok {
+		// Q 此处是否有强制类型转换
+		return element.Value.(*SessionStore), nil
+	} else {
+		// 没有sid对应的Session则创建新的Session
+		sess, err := pder.SessionInit(sid)
+		return sess, err
+	}
+	// 此行代码是否多余  就是多余！
+	// return nil,nil
+}
+
+func (pder *Provider) SessionDestory(sid string) error {
+	if element, ok := pder.sessions[sid]; ok {
+		// 删除指针
+		delete(pder.sessions, sid)
+		// 删除真实的session list Element
+		pder.list.Remove(element)
+	}
+	return nil
+}
+
+func (pder *Provider) SessionGC(maxlifetime int64) {
+	pder.lock.Lock()
+	defer pder.lock.Unlock()
+	for {
+		// 遍历 provider 中的element 元素 检查是否过期？
+		element := pder.list.Back()
+		if element == nil {
+			break
+		}
+		//timeAccessed (type Time) 的方法
+		if element.Value.(*SessionStore).timeAccessed.Unix()+maxlifetime < time.Now().Unix() {
+			pder.list.Remove(element)
+			delete(pder.sessions, element.Value.(*SessionStore).sid)
+		} else {
+			break
+		}
+	}
+}
+
+// Provider 接口 以外的方法
+func (pder *Provider) SessionUpdate(sid string) error {
+	pder.lock.Lock()
+	defer pder.lock.Unlock()
+
+	if element, ok := pder.sessions[sid]; ok {
+		element.Value.(*SessionStore).timeAccessed = time.Now()
+		pder.list.MoveToFront(element)
+		return nil
+	}
+	return nil
+}
+
+func init() {
+	pder.sessions = make(map[string]*list.Element, 0)
+	session.Register("memory", pder)
+}
 
 type SessionStore struct {
 	// session id 唯一的表示
@@ -36,12 +101,13 @@ type SessionStore struct {
 func (st *SessionStore) Set(key, value interface{}) error {
 	st.value[key] = value
 	// Todo pder update
-	pder.list
+	pder.SessionUpdate(st.sid)
 	return nil
 }
 
 func (st *SessionStore) Get(key interface{}) interface{} {
 	// Todo pder update
+	pder.SessionUpdate(st.sid)
 	if v, ok := st.value[key]; ok {
 		return v
 	} else {
@@ -50,7 +116,12 @@ func (st *SessionStore) Get(key interface{}) interface{} {
 }
 
 func (st *SessionStore) Delete(key interface{}) error {
+	// 删除某个Session值
 	delete(st.value, key)
-	// ToDO update
+	pder.SessionUpdate(st.sid)
 	return nil
+}
+
+func (st *SessionStore) SessionID() string {
+	return st.sid
 }
