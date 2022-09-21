e3ca20dc2ee66c29a8bc6fadeff8359379977175
GitFLHub <1390491039@qq.com>
2022-09-21 19:41:15 +0800

    v0.0.4

--- web/session/session.go
+++ web/session/session.go
@@ -3,7 +3,6 @@ package session
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -12,86 +11,36 @@ import (
 )
 
 type Session interface {
-	Set(key, value interface{}) error //set session value
-	Get(key interface{}) interface{}  //get session value
-	Delete(key interface{}) error     //delete session value
-	SessionID() string                //back current sessionID
+	Set(key, value interface{}) error
+	Get(key interface{}) interface{}
+	Delete(key interface{}) error
+	SessionID() string
 }
 
 type Provider interface {
 	SessionInit(sid string) (Session, error)
 	SessionRead(sid string) (Session, error)
-	SessionDestroy(sid string) error
+	SessionDestory(sid string) error
 	SessionGC(maxlifetime int64)
 }
 
-var provides = make(map[string]Provider)
+var providers = make(map[string]Provider)
 
-// Register makes a session provide available by the provided name.
-// If Register is called twice with the same name or if driver is nil,
-// it panics.
-func Register(name string, provide Provider) {
-	if provide == nil {
+func Register(name string, provider Provider) {
+	if provider == nil {
 		panic("session: Register provide is nil")
 	}
-	if _, dup := provides[name]; dup {
+	if _, dup := providers[name]; dup {
 		panic("session: Register called twice for provide " + name)
 	}
-	provides[name] = provide
+	providers[name] = provider
 }
 
 type Manager struct {
-	cookieName  string     //private cookiename
-	lock        sync.Mutex // protects session
+	cookieName  string //
+	lock        sync.Mutex
 	provider    Provider
 	maxlifetime int64
-}
-
-func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
-	provider, ok := provides[provideName]
-	if !ok {
-		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
-	}
-	return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
-}
-
-// get Session
-func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
-	manager.lock.Lock()
-	defer manager.lock.Unlock()
-	cookie, err := r.Cookie(manager.cookieName)
-	if err != nil || cookie.Value == "" {
-		sid := manager.sessionId()
-		session, _ = manager.provider.SessionInit(sid)
-		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxlifetime)}
-		http.SetCookie(w, &cookie)
-	} else {
-		sid, _ := url.QueryUnescape(cookie.Value)
-		session, _ = manager.provider.SessionRead(sid)
-	}
-	return
-}
-
-// Destroy sessionid
-func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie(manager.cookieName)
-	if err != nil || cookie.Value == "" {
-		return
-	} else {
-		manager.lock.Lock()
-		defer manager.lock.Unlock()
-		manager.provider.SessionDestroy(cookie.Value)
-		expiration := time.Now()
-		cookie := http.Cookie{Name: manager.cookieName, Path: "/", HttpOnly: true, Expires: expiration, MaxAge: -1}
-		http.SetCookie(w, &cookie)
-	}
-}
-
-func (manager *Manager) GC() {
-	manager.lock.Lock()
-	defer manager.lock.Unlock()
-	manager.provider.SessionGC(manager.maxlifetime)
-	time.AfterFunc(time.Duration(manager.maxlifetime)*time.Second, func() { manager.GC() })
 }
 
 func (manager *Manager) sessionId() string {
@@ -100,4 +49,61 @@ func (manager *Manager) sessionId() string {
 		return ""
 	}
 	return base64.URLEncoding.EncodeToString(b)
+}
+
+func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	cookie, err := r.Cookie(manager.cookieName)
+	// err 用来判断有无cookie
+	if err != nil || cookie.Value == "" {
+		// 没有便创建
+		sid := manager.sessionId()
+		// 此处的session 并不是新创建的，而是默认的返回值
+		session, _ = manager.provider.SessionInit(sid)
+		cookie := http.Cookie{
+			Name: manager.cookieName,
+			// 将 sid 经过 url.QueryEscape() 后放入 cookie
+			Value:    url.QueryEscape(sid),
+			Path:     "/",
+			HttpOnly: true,
+			MaxAge:   int(manager.maxlifetime),
+		}
+		http.SetCookie(w, &cookie)
+	} else {
+		// 如何查询已有的 sessionID
+		sid, _ := url.QueryUnescape(cookie.Value)
+		// 读取session
+		session, _ = manager.provider.SessionRead(sid)
+	}
+	return
+}
+
+func (manager *Manager) SessionDestory(w http.ResponseWriter, r http.Request) {
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		return
+	} else {
+		manager.lock.Lock()
+		defer manager.lock.Unlock()
+		manager.provider.SessionDestory(cookie.Value)
+		expiration := time.Now()
+		cookie := http.Cookie{
+			Name:     manager.cookieName,
+			Path:     "/",
+			HttpOnly: true,
+			Expires:  expiration,
+			MaxAge:   -1,
+		}
+		http.SetCookie(w, &cookie)
+	}
+}
+
+func (manager *Manager) GC() {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	// SessionGC 需要看具体的实现
+	manager.provider.SessionGC(manager.maxlifetime)
+	// 定时进行GC
+	time.AfterFunc(time.Duration(manager.maxlifetime)*time.Second, func() { manager.GC() })
 }
